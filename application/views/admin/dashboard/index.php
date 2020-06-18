bece1bc5157a3baa59413e353d1d5ee911d0942c
ridwanzal <zalbinaridwan@gmail.com>
2020-06-18 22:06:32 +0700

    hide graph

--- application/views/admin/dashboard/index.php
+++ application/views/admin/dashboard/index.php
@@ -92,7 +92,7 @@
             </div>
         </div>
     </section> -->
-    <div class="line-chart-area">
+    <div class="line-chart-area" style="display:none;">
         <div class="container">
             <div class="row">
                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
